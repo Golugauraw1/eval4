2b10fdbd8368eaf139ed700b9508ac8c72f7b4a4
golugauraw1 <kavermaji89@gmail.com>
2023-07-17 14:15:10 +0530

    final

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,22 +1,22 @@
 require("dotenv").config()
-const jwt=require("jsonwebtoken")
+const jwt = require("jsonwebtoken")
 
-const authentication=(req,res,next)=>{
-    const token=req.headers.authorization
-    if(token){
-        const decoded=jwt.verify(token,process.env.key)
-        if(decoded){
-            const userID=decoded.userID
+const authentication = (req, res, next) => {
+    const token = req.headers.authorization
+    if (token) {
+        const decoded = jwt.verify(token, process.env.key)
+        if (decoded) {
+            const userID = decoded.userID
             console.log(decoded)
-            req.body.userID=userID
+            req.body.userID = userID
             next()
         }
-        else{
+        else {
             res.send("Login is required first")
         }
     }
-    else{
+    else {
         res.send("Login first")
     }
 }
-module.exports={authentication}
+module.exports = { authentication }
--- model/post.moel.js
+++ model/post.moel.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 const postSchema = mongoose.Schema({
-    title: String,
+  title: String,
   body: String,
   device: String,
- 
+
 });
 
 const postModel = mongoose.model("post", postSchema);
 
-module.exports =postModel
-;
+module.exports = postModel
+  ;
